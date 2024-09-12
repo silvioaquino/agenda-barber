3b5b15b6899f4107da6b36db0437d8e3c1bad723
Silvio Aquino <silvioaquino@Silvios-MacBook-Pro.local>
2024-09-11 22:56:55 -0300

    feat: add Header compent and banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,36 @@
-"use client"
-
+import { Input } from "./_components/ui/input"
+import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
-
-// SEVER COMPONENTS
+import { SearchIcon } from "lucide-react"
+import Image from "next/image"
 
 const Home = () => {
-  return <Button>test</Button>
+  return (
+    <div>
+      {/* header */}
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Silvio</h2>
+        <p>Segunda-feira, 05 de agosto.</p>
+
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative mt-6 h-[160px] w-full">
+          <Image
+            alt="Agende nos melhores com FSW Barber"
+            src="/banner01.png"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Home
