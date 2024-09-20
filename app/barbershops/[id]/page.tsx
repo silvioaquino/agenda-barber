74324cb7cb2f8c4d8a09cd3f1f82b64344e9e237
Silvio Aquino <silvioaquino@Silvios-MacBook-Pro.local>
2024-09-20 14:04:37 -0300

    feat: add serviceItem components

--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -3,6 +3,7 @@ import { db } from "@/app/_lib/prisma"
 //import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react";
 //import Image from "next/image"
 import BarbershopInfo from "./_components/barbershop-info"
+import ServiceItem from "./_components/service-item"
 
 interface BarbershopDetailPageProps {
   params: {
@@ -20,6 +21,9 @@ const BarbershopDetailPage = async ({ params }: BarbershopDetailPageProps) => {
     where: {
       id: params.id,
     },
+    include: {
+      services: true,
+    },
   })
 
   if (!barbershop) {
@@ -27,7 +31,15 @@ const BarbershopDetailPage = async ({ params }: BarbershopDetailPageProps) => {
     return null
   }
 
-  return <BarbershopInfo barbershop={barbershop} />
+  return (
+    <div className="flex flex-col gap-4 py-6">
+      <BarbershopInfo barbershop={barbershop} />
+
+      {barbershop.services.map((service) => (
+        <ServiceItem key={service.id} service={service} />
+      ))}
+    </div>
+  )
 }
 
 export default BarbershopDetailPage
